b28ab87addf8dee94bbf356442754a0ace08a1bd
root <root@ip-172-31-16-168.eu-north-1.compute.internal>
2023-11-27 08:01:24 +0000

    executing if else in bashscripts

---
+++ pgm5.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+echo "please enter the value of a"
+read a
+echo "please enter the value of b"
+read b
+
+if [ $a == $b ]
+then
+   echo "a is equal to b"
+elif [ $a -gt $b ]
+then
+   echo "a is greater than b"
+elif [ $a -lt $b ]
+then
+   echo "a is less than b"
+else
+   echo "None of the condition met"
+fi
